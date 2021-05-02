5ad017485559204ede16f2e24410b2c79310751c
caseymccullough <kcmccullough@gmail.com>
2021-05-02 15:18:34 -0400

    adding all React files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <h1>Bookmarks App</h1>
+    </div>
+  );
+}
+
+export default App;
